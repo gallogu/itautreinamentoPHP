0d1b11320a0e2d2bf6beb84a81c0bad0d69189e3
gallogu <gustavosgallo@gmail.com>
2021-01-14 14:40:29 -0300

    versionando novo html

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-Aula ensinando a fazer um deploy simples
+Example of Texto em negrito
+
+Example of Texto enfatizado
+
+Example of Texto forte
+
+Example of Texto em italico
+
+Example of Texto superior
+
+Example of Texto inferior
+
+Example of Texto taxado
+
+Example of Texto de código de computador
