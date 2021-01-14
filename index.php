7ed07340d28a32c8d3f23f2d72fdfad4f48d554c
gallogu <gustavosgallo@gmail.com>
2021-01-14 14:43:22 -0300

    versionando novo html

--- index.php
+++ index.php
@@ -1,15 +1,8 @@
-Example of Texto em negrito
-
-Example of Texto enfatizado
-
-Example of Texto forte
-
-Example of Texto em italico
-
-Example of Texto superior
-
-Example of Texto inferior
-
-Example of Texto taxado
-
-Example of Texto de código de computador
+<dl>
+  <dt>solilóquio</dt>
+  <dd>No drama, onde um personagem fala a si mesmo, representando seus pensamentos ou sentimentos internos e no processo, transmitindo-os ao público (mas não a outros personagens).</dd>
+  <dt>monólogo</dt>
+  <dd>No drama, onde um personagem fala seus pensamentos em voz alta para compartilhá-los com o público e com outros personagens presentes.</dd>
+  <dt>aparte</dt>
+  <dd>No drama, onde um personagem compartilha um comentário apenas com o público para efeito humorístico ou dramático. Isso geralmente é um sentimento, pensamento ou parte de informação de fundo adicional.</dd>
+</dl>
